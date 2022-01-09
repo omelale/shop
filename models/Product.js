f4ace322881847de3a6a2432f8340af02b5af638
Ajten <ajtencani@gmail.com>
2022-01-09 16:04:38 +0100

    defined Product Model

---
+++ models/Product.js
@@ -0,0 +1,12 @@
+class Product {
+    constructor(id, title,ownerId,imageUrl,description, price) {
+        this.id = id;
+        this.title = title;
+        this.ownerId = ownerId;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
+    }
+}
+
+export default Product
