540e591ea7d0005853190a16699666e8aa8cccfd
Ajten <ajtencani@gmail.com>
2022-01-10 00:10:49 +0100

    navigate from cart to order screen

--- screens/store/CartScreen.js
+++ screens/store/CartScreen.js
@@ -1,10 +1,17 @@
 import React from "react";
-import {StyleSheet,Text,View} from "react-native";
+import {StyleSheet,Text,View,Button} from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const CartScreen = () => {
+
+const CartScreen = (props) => {
     return (
         <View style={styles.container}>
           <Text style={styles.txt}>This is the Cart screen</Text>
+          <Button
+            title="Go to Orders Screen"
+            onPress={() => props.navigation.navigate('OrdersScreen')}
+          />
         </View>)
 }
 
