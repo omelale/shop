20f8c74d7f6662ac6aa0f4a2ea90f7b4a2379cb9
Ajten <ajtencani@gmail.com>
2022-01-09 23:09:24 +0100

    initiated anew tab navigator and added to main app

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React, {useState} from "react";
-import 'react-native-gesture-handler';
 import {StyleSheet, Text, View} from 'react-native';
-import HomeScreen from "./screens/store/HomeScreen";
+import {NavigationContainer} from '@react-navigation/native';
 import {combineReducers, createStore} from "redux";
 import {Provider} from "react-redux";
 import productsReducer from "./store/reducers/products";
 import AppLoading from 'expo-app-loading';
 import useFonts from './hooks/useFont';
+import TabMainNavigation from "./navigation/TabMainNavigation";
 
 
 
@@ -31,22 +31,8 @@ export default function App() {
         />);
     }
     return (
-        <Provider store={store}><View style={styles.container}>
-            {/*<HomeScreen/>*/}
-            <Text style={styles.test}>Init</Text>
-        </View></Provider>
+        <Provider store={store}><NavigationContainer>
+        <TabMainNavigation/>
+        </NavigationContainer></Provider>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    test: {
-        fontFamily: 'SplineSans-Bold',
-        fontSize:40
-    }
-});
---
+++ navigation/TabMainNavigation.js
@@ -0,0 +1,41 @@
+import homeScreen from "../screens/store/HomeScreen";
+import cartScreen from "../screens/store/CartScreen";
+import searchScreen from "../screens/store/SearchScreen";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import {Ionicons} from "@expo/vector-icons";
+import colors from "../constants/colors";
+
+
+const Tab = createBottomTabNavigator();
+
+function TabMainNavigation() {
+    return (
+        <Tab.Navigator
+            screenOptions={({route}) => ({
+                tabBarIcon: ({focused, color, size}) => {
+                    let iconName;
+                    if (route.name === 'Home') {
+                        iconName = focused
+                            ? 'ios-home'
+                            : 'ios-home-outline';
+                    } else if (route.name === 'Search') {
+                        iconName = focused ? 'ios-search-circle' : 'ios-search-circle-outline';
+                    } else if (route.name === 'Cart') {
+                        iconName = focused ? 'ios-cart' : 'ios-cart-outline';
+                    }
+                    size = 27;
+                    // You can return any component that you like here!
+                    return <Ionicons name={iconName} size={size} color={color}/>;
+                },
+                tabBarActiveTintColor: colors.primaryColor,
+                tabBarInactiveTintColor: colors.primaryColor,
+            })}
+        >
+            <Tab.Screen name="Home" component={homeScreen} options={{headerShown: false}}/>
+            <Tab.Screen name="Search" component={searchScreen} options={{headerShown: false}}/>
+            <Tab.Screen name="Cart" component={cartScreen} options={{headerShown: false}}/>
+        </Tab.Navigator>
+    );
+}
+
+export default TabMainNavigation
