6da9a5da1a2a35788d4a1c50578c22a3eb58f15b
Ajten <ajtencani@gmail.com>
2022-01-09 17:32:57 +0100

    reformat

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
+import React from "react";
 import { StyleSheet, Text, View } from 'react-native';
 import HomeScreen from "./screens/store/HomeScreen";
+import {combineReducers, createStore} from "redux";
+import {Provider} from "react-redux";
+import productsReducer from "./store/reducers/products";
+import AppLoading from 'expo-app-loading';
+
+
+const rootReducer = combineReducers({
+    products: productsReducer
+});
+const store = createStore(rootReducer);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <HomeScreen></HomeScreen>
-    </View>
+      <Provider store={store}><View style={styles.container}>
+        <HomeScreen></HomeScreen>
+      </View></Provider>
   );
 }
 
