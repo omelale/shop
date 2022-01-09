6a769e6fe8d5dbde832d3d942538e708be4fde12
Ajten <ajtencani@gmail.com>
2022-01-09 16:15:01 +0100

    initialised a basic non mutating reducer for products

---
+++ store/reducers/products.js
@@ -0,0 +1,10 @@
+const initialState = {
+    availableProducts : [],
+    userProducts: []
+}
+
+const productReducer = (state=initialState,action) => {
+    return state;
+}
+
+export default productReducer
