fa3e1b64aeacc5681cfe199fa358bb367c573547
Ajten <ajtencani@gmail.com>
2022-01-09 16:47:56 +0100

    small change in order of definition of properties in product model to have correct implementation of dummy data

--- models/Product.js
+++ models/Product.js
@@ -1,8 +1,8 @@
 class Product {
     constructor(id, title,ownerId,imageUrl,description, price) {
         this.id = id;
-        this.title = title;
         this.ownerId = ownerId;
+        this.title = title;
         this.imageUrl = imageUrl;
         this.description = description;
         this.price = price;
