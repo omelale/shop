9bd1296425350741f6b84636f5b59d06ee128f03
Ajten <ajtencani@gmail.com>
2022-01-10 23:15:36 +0100

    added new component to display single product

---
+++ components/shop/ProductItem.js
@@ -0,0 +1,77 @@
+import React from "react";
+import {View, Text, Image, StyleSheet, Button} from 'react-native'
+import Colors from "../../constants/colors";
+
+const ProductItem = (props) => {
+console.log(props.product.imageUrl)
+return (
+    <View style={styles.product}>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={{ uri: props.product.imageUrl }} />
+      </View>
+      <View style={styles.details}>
+        <Text style={styles.title}>{props.product.title}</Text>
+        <Text style={styles.price}>${props.product.price.toFixed(2)}</Text>
+      </View>
+      <View style={styles.actions}>
+        <Button
+          color={Colors.primary}
+          title="View Details"
+          onPress={props.onViewDetails}
+        />
+        <Button
+          color={Colors.primary}
+          title="To Cart"
+          onPress={props.onAddToCart}
+        />
+      </View>
+    </View>
+)
+}
+
+const styles = StyleSheet.create({
+    product: {
+    shadowColor: 'black',
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    elevation: 5,
+    borderRadius: 10,
+    backgroundColor: 'white',
+    height: 300,
+    margin: 20
+  },
+  imageContainer: {
+    width: '100%',
+    height: '60%',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    overflow: 'hidden'
+  },
+  image: {
+    width: '100%',
+    height: '100%'
+  },
+  details: {
+    alignItems: 'center',
+    height: '19%',
+    padding: 10
+  },
+  title: {
+    fontSize: 18,
+    marginVertical: 4
+  },
+  price: {
+    fontSize: 14,
+    color: '#888'
+  },
+  actions: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: '25%',
+    paddingHorizontal: 20
+  }
+})
+
+export default ProductItem
