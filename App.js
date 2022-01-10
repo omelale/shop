eba8e0ae7d53f5e42e188ef7e3f2e18cd69aa596
Ajten <ajtencani@gmail.com>
2022-01-10 22:02:54 +0100

    removed unused dependencies

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React, {useState} from "react";
-import {StyleSheet, Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {combineReducers, createStore} from "redux";
 import {Provider} from "react-redux";
