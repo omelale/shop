4a42dc462b2c9e4d9c7f284b0fa3257d3c6f24ae
Ajten <ajtencani@gmail.com>
2022-01-11 20:28:48 +0100

    added remove from cart action

--- store/actions/cart.js
+++ store/actions/cart.js
@@ -1,4 +1,12 @@
 export  const ADD_TO_CART = 'ADD_TO_CART';
+export const  REMOVE_FROM_CART = 'REMOVE_FROM_CART';
+
+export const removeFromCart = (product) => {
+    return {
+        type : REMOVE_FROM_CART,
+        product : product
+    }
+}
 
 export const addToCart = (product) => {
     return {
