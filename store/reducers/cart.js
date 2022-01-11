d9e8e2a56b5064033251bf43691a2a4d4856d28b
Ajten <ajtencani@gmail.com>
2022-01-11 19:45:17 +0100

    added initial state to reducer

---
+++ store/reducers/cart.js
@@ -0,0 +1,11 @@
+const initialState = {
+    totalProducts : 0,
+    items : {},
+    totalAmount : 0
+}
+
+const cartReducer = (state = initialState, action) => {
+    return state
+}
+
+export default cartReducer
