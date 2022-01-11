eae9ec2fd1792dae43c8e4a1830404217b9657a7
Ajten <ajtencani@gmail.com>
2022-01-12 00:35:26 +0100

    added an add to favourites reducer response

--- store/reducers/products.js
+++ store/reducers/products.js
@@ -1,12 +1,30 @@
 import PRODUCTS from "../../data/dummy-data";
+import {ADD_TO_FAVOURITES} from "../actions/products";
 
 const initialState = {
     availableProducts : PRODUCTS,
+    favouriteProducts : [],
     userProducts: PRODUCTS.filter(product=>product.ownerId === 'u1')
 }
 
 const productReducer = (state=initialState,action) => {
-    return state;
+    switch (action.type) {
+        case ADD_TO_FAVOURITES : {
+            const addedProduct = action.product;
+            const existingIndex = state.favouriteProducts.findIndex(product => product.id === addedProduct.id);
+            if(existingIndex >= 0){
+                const newFavs = [...state.favouriteProducts];
+                // const removed = newFavs.splice(existingIndex,1);
+                const removed = newFavs.filter(product => product.id !== addedProduct.id)
+                return {...state, favouriteProducts: removed}
+            } else {
+                return {...state,favouriteProducts: state.favouriteProducts.concat(state.availableProducts.find(product=>product.id===addedProduct.id))}
+            }
+        }
+        default : {
+            return state;
+        }
+    }
 }
 
 export default productReducer
