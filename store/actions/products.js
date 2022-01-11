52de52cb99382986a98124b5d7c24c6fef8d96e4
Ajten <ajtencani@gmail.com>
2022-01-12 00:35:02 +0100

    added an add to favourites action

---
+++ store/actions/products.js
@@ -0,0 +1,8 @@
+export const ADD_TO_FAVOURITES = 'ADD_TO_FAVOURITES';
+
+export const addToFavourites = (product) => {
+    return {
+        type : ADD_TO_FAVOURITES,
+        product : product
+    }
+}
