d8df28ed88e11ce359ee9488dd4cf395c8f9189f
Ajten <ajtencani@gmail.com>
2022-01-13 00:32:24 +0100

    added product favourites screen

--- screens/user/FavouriteProducts.js
+++ screens/user/FavouriteProducts.js
@@ -1,8 +1,26 @@
 import React from "react";
-import {StyleSheet,Text,View} from "react-native";
+import {StyleSheet,Text,View,FlatList} from "react-native";
+import {useSelector,useDispatch} from "react-redux";
+import ProductItem from "../../components/shop/ProductItem";
+import * as cartActions from '../../store/actions/cart'
 
-const FavouriteProducts = () => {
-    return (<View className={styles.container}><Text>This is the favourite products list screen</Text></View>)
+
+const FavouriteProducts = (props) => {
+  const favProds = useSelector(state => state.products.favouriteProducts);
+      const dispatch = useDispatch()
+    return (
+        <FlatList
+            data={favProds}
+            renderItem={ itemData =>
+                <ProductItem
+                    product={itemData.item}
+                onViewDetails={() => {
+                    props.navigation.navigate('ProductDetailsScreen', {product: itemData.item});
+                }}
+                onAddToCart={() => { dispatch(cartActions.addToCart(itemData.item))}}
+                /> }
+        />
+    )
 }
 
 const styles = StyleSheet.create({
