7caaa5d63d92972df320b4cda778c83a51058981
Ajten <ajtencani@gmail.com>
2022-01-13 00:52:46 +0100

    added a side drawer for favs

--- navigation/SideDrawerNavigation.js
+++ navigation/SideDrawerNavigation.js
@@ -3,14 +3,16 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import ProductListScreen from "../screens/store/ProductsListScreen";
 import FavouriteProducts from "../screens/user/FavouriteProducts";
+import {ProductStackNavigator} from "./ProductsStackNavigator";
+import HomeScreen from "../screens/store/HomeScreen";
 
 const Drawer = createDrawerNavigator();
 
 const SideDrawerNavigation = () => {
     return (
-    <NavigationContainer>
+    <NavigationContainer independent={true}>
       <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={ProductListScreen} />
+        <Drawer.Screen name="Home" component={ProductStackNavigator} />
         <Drawer.Screen name="FavouriteProducts" component={FavouriteProducts} />
       </Drawer.Navigator>
     </NavigationContainer>
