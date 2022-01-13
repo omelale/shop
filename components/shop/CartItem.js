154799f29dc670516ac3b57c97859a8b873131d0
Ajten <ajtencani@gmail.com>
2022-01-13 22:33:08 +0100

    added styles

---
+++ components/shop/CartItem.js
@@ -0,0 +1,38 @@
+import React from "react";
+import {StyleSheet, View} from "react-native";
+
+const CartItem = (props) => {
+    return (
+        <View>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    cartItem: {
+    padding: 10,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 20
+  },
+  itemData: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  quantity: {
+    fontFamily: 'open-sans',
+    color: '#888',
+    fontSize: 16
+  },
+  mainText: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 16
+  },
+  deleteButton: {
+    marginLeft: 20
+  }
+});
+
+export default CartItem;
