57dee269ee820da7ad38a91b5a6ccb7c4064ed1d
Ajten <ajtencani@gmail.com>
2022-01-14 21:33:42 +0100

    fix old drawer

--- navigation/SideDrawerNavigation.js
+++ navigation/SideDrawerNavigation.js
@@ -1,22 +1,20 @@
 import * as React from 'react';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
-import ProductListScreen from "../screens/store/ProductsListScreen";
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {NavigationContainer} from '@react-navigation/native';
 import FavouriteProducts from "../screens/user/FavouriteProducts";
-import {ProductStackNavigator} from "./ProductsStackNavigator";
-import HomeScreen from "../screens/store/HomeScreen";
+import OrdersScreen from "../screens/store/OrdersScreen";
 
 const Drawer = createDrawerNavigator();
 
 const SideDrawerNavigation = () => {
     return (
-    <NavigationContainer independent={true}>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={ProductStackNavigator} />
-        <Drawer.Screen name="FavouriteProducts" component={FavouriteProducts} />
-      </Drawer.Navigator>
-    </NavigationContainer>
-  );
+        <NavigationContainer independent={true}>
+            <Drawer.Navigator initialRouteName="OrdersScreen">
+                <Drawer.Screen name="OrdersScreen" component={OrdersScreen}/>
+                <Drawer.Screen name="FavouriteProducts" component={FavouriteProducts}/>
+            </Drawer.Navigator>
+        </NavigationContainer>
+    );
 }
 
 export default SideDrawerNavigation
