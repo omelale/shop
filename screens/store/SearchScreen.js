7a5bec2e7bd3549a6e45b8861eb496558d585f75
Ajten <ajtencani@gmail.com>
2022-01-15 00:53:20 +0100

    added a new search screen

--- screens/store/SearchScreen.js
+++ screens/store/SearchScreen.js
@@ -1,16 +1,51 @@
-import React from "react";
-import {StyleSheet,Text,View} from "react-native";
+import React, {useEffect, useState} from "react";
+import {useSelector} from "react-redux";
+import {SafeAreaView, StyleSheet, Text,View} from "react-native";
+import SearchList from "../../components/shop/SearchList";
+import SearchBar from "../../components/shop/SearchBar";
 
-const SearchScreen = () => {
-    return (<View className={styles.container}><Text>This is the search screen</Text></View>)
-}
+const SearchScreen = (props) => {
+    const [searchPhrase, setSearchPhrase] = useState("");
+    const [clicked, setClicked] = useState(false);
+    // const [fakeData, setFakeData] = useState();
+    const products = useSelector(state=>state.products.availableProducts);
+    // get data from the fake api endpoint
+    // useEffect(() => {
+    //     const getData = async () => {
+    //         const apiResponse = await fetch(
+    //             "https://my-json-server.typicode.com/kevintomas1995/logRocket_searchBar/languages"
+    //         );
+    //         const data = await apiResponse.json();
+    //         setFakeData(data);
+    //     };
+    //     getData();
+    // }, []);
+
+    return (
+        <SafeAreaView style={styles.root}>
+            {/*{!clicked && <Text style={styles.title}>Search for a product</Text>}*/}
+            <SearchBar
+                searchPhrase={searchPhrase}
+                setSearchPhrase={setSearchPhrase}
+                clicked={clicked}
+                setClicked={setClicked}
+                style={styles.search}
+            />
+            <SearchList
+                searchPhrase={searchPhrase}
+                data={products}
+                setClicked={setClicked}
+                navigation={props.navigation}
+            />
+        </SafeAreaView>
+    );
+};
+
+export default SearchScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    root: {
+        justifyContent: "center",
+        alignItems: "center",
+    }
 });
-export default SearchScreen
