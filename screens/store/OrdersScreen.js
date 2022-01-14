12c2498232663230dcd5da51cb220ca8b54bb3c9
Ajten <ajtencani@gmail.com>
2022-01-14 21:31:50 +0100

    fix right way to get the order

--- screens/store/OrdersScreen.js
+++ screens/store/OrdersScreen.js
@@ -1,24 +1,24 @@
 import React from "react";
-import {StyleSheet,Text,View,FlatList} from "react-native";
+import {FlatList, StyleSheet, Text} from "react-native";
 import {useSelector} from "react-redux";
 
 const OrdersScreen = (props) => {
-    const orders = useSelector(state => state.orders.orders);
+    const orders = useSelector(state => state.order.orders);
     return (
-        <FlatList data={orders} renderItem={item => <Text>{item.totalAmount}</Text>}/>
+        <FlatList data={orders} renderItem={item => <Text>{item.item.totalAmount}</Text>}/>
     )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-   txt : {
-    fontSize:20,
-    fontFamily:'SplineSans-Bold'
-  }
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    txt: {
+        fontSize: 20,
+        fontFamily: 'SplineSans-Bold'
+    }
 });
 export default OrdersScreen
