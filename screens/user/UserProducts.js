4b1de37ec7b30a881672418a944f5d36f1da58c4
Ajten <ajtencani@gmail.com>
2022-01-15 18:04:02 +0100

    modified layout to show user products

--- screens/user/UserProducts.js
+++ screens/user/UserProducts.js
@@ -1,16 +1,28 @@
 import React from "react";
-import {StyleSheet,Text,View} from "react-native";
+import {FlatList} from "react-native";
+import ProductItem from "../../components/shop/ProductItem";
+import {useDispatch, useSelector} from "react-redux";
+import * as cartActions from '../../store/actions/cart'
 
-const UserProducts = () => {
-    return (<View className={styles.container}><Text>This is the user products list screen</Text></View>)
+
+const UserProducts = (props) => {
+    const userProducts = useSelector(state => state.products.userProducts);
+    const dispatch = useDispatch();
+    return (
+        <FlatList
+            data={userProducts}
+            renderItem={itemData =>
+                <ProductItem
+                    product={itemData.item}
+                    onViewDetails={() => {
+                        props.navigation.navigate('ProductDetailsScreen', {product: itemData.item});
+                    }}
+                    onAddToCart={() => {
+                        dispatch(cartActions.addToCart(itemData.item))
+                    }}
+                />}
+        />
+    )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 export default UserProducts
