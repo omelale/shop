4f702e1b7d9dc8d6f2bb7f076d6955e7eec99107
Ajten <ajtencani@gmail.com>
2022-01-16 00:09:10 +0100

    created an edit product action

--- store/actions/products.js
+++ store/actions/products.js
@@ -1,6 +1,7 @@
 export const ADD_TO_FAVOURITES = 'ADD_TO_FAVOURITES';
 export const DELETE_PRODUCT = 'DELETE_PRODUCT';
 export const CREATE_PRODUCT = 'CREATE_PRODUCT';
+export const EDIT_PRODUCT = 'EDIT_PRODUCT';
 
 export const addToFavourites = (product) => {
     return {
@@ -19,6 +20,13 @@ export const deleteProduct = (product)=>{
 export const createProduct = (product) => {
     return {
         type : CREATE_PRODUCT,
+        product: product
+    }
+}
+
+export const editProduct = (product) => {
+    return {
+        type : EDIT_PRODUCT,
         product: product
     }
 }
