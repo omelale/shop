e91ffc45305507ac4bf65fff140a54b294c44c3f
Ajten <ajtencani@gmail.com>
2022-01-16 00:05:58 +0100

    added button to go to add product

--- screens/user/UserProducts.js
+++ screens/user/UserProducts.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React , {useLayoutEffect}from "react";
 import {FlatList} from "react-native";
 import ProductItem from "../../components/shop/ProductItem";
 import {useDispatch, useSelector} from "react-redux";
 import * as cartActions from '../../store/actions/cart'
 import * as productActions from '../../store/actions/products'
-
+import {HeaderButtons, Item} from "react-navigation-header-buttons";
+import HeaderButton from "../../components/HeaderButton";
 
 const UserProducts = (props) => {
     const userProducts = useSelector(state => state.products.userProducts);
     const dispatch = useDispatch();
+    useLayoutEffect(
+        () => {
+            props.navigation.setOptions({
+                headerRight: () => (
+                    <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                        <Item
+                            iconName='ios-add-circle-outline'
+                            onPress={() => {
+                                props.navigation.navigate('addProducts')
+                            }}
+                        />
+                    </HeaderButtons>
+                )
+            })
+        }
+    );
     return (
         <FlatList
             data={userProducts}
