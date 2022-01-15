a7e87b550614b3b1a9f4259e87dc7ceb958d196d
Ajten <ajtencani@gmail.com>
2022-01-15 18:43:20 +0100

    setup action for deleting product

--- store/actions/products.js
+++ store/actions/products.js
@@ -1,8 +1,16 @@
 export const ADD_TO_FAVOURITES = 'ADD_TO_FAVOURITES';
+export const DELETE_PRODUCT = 'DELETE_PRODUCT';
 
 export const addToFavourites = (product) => {
     return {
         type: ADD_TO_FAVOURITES,
+        product: product
+    }
+}
+
+export const deleteProduct = (product)=>{
+    return {
+        type: DELETE_PRODUCT,
         product: product
     }
 }
