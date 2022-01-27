0d342c8cea308c7fa650cc5b1dfc3f12d45b91c8
Ajten <ajtencani@gmail.com>
2022-01-27 21:02:34 +0100

    converted the create action to async and connect to DB

--- store/actions/products.js
+++ store/actions/products.js
@@ -1,3 +1,5 @@
+import Product from "../../models/Product";
+
 export const ADD_TO_FAVOURITES = 'ADD_TO_FAVOURITES';
 export const DELETE_PRODUCT = 'DELETE_PRODUCT';
 export const CREATE_PRODUCT = 'CREATE_PRODUCT';
@@ -16,8 +18,33 @@ export const deleteProduct = (product) => {
 }
 
 export const createProduct = (product) => {
-    return {
-        type: CREATE_PRODUCT, product: product
+    return async dispatch => {
+        //here we can put any async code we want
+        const response = await fetch('https://shop-348b2-default-rtdb.europe-west1.firebasedatabase.app/products.json', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body:
+                JSON.stringify(
+                    {
+                        title : product.title,
+                        description: product.description,
+                        imageUrl : product.imageUrl,
+                        price : product.price
+                    }
+                )
+        });
+
+        const resData = await response.json();
+        console.log(resData);
+        console.log(product);
+        let prod = {...product}
+        prod.id = resData.name
+        console.log(prod);
+        dispatch({
+            type: CREATE_PRODUCT, product: prod
+        });
     }
 }
 
