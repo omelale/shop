b2bde0a03da7a8afc000854d2255a16282e4f452
Ajten <ajtencani@gmail.com>
2022-01-28 14:52:40 +0100

    added a loading spinner when retrieving all products from database

--- screens/store/ProductsListScreen.js
+++ screens/store/ProductsListScreen.js
@@ -1,16 +1,31 @@
-import React, {useEffect} from "react";
-import {FlatList, StyleSheet} from "react-native";
+import React, {useEffect, useState} from "react";
+import {ActivityIndicator, FlatList, StyleSheet, View} from "react-native";
 import {useDispatch, useSelector} from "react-redux";
 import ProductItem from "../../components/shop/ProductItem";
 import * as cartActions from '../../store/actions/cart'
 import * as productActions from "../../store/actions/products";
+import Colors from "../../constants/colors";
 
 const ProductListScreen = (props) => {
     const products = useSelector(state => state.products.availableProducts);
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
+    const [isLoading, setIsLoading] = useState(false);
     useEffect(() => {
-        dispatch(productActions.fetchProducts());
-    },[dispatch])
+        const loadProducts = async () => {
+            setIsLoading(true);
+            await dispatch(productActions.fetchProducts());
+            setIsLoading(false);
+        }
+        loadProducts();
+    }, [dispatch])
+
+    if (isLoading) {
+        return (
+            <View style={styles.centered}>
+                <ActivityIndicator size='large' color={Colors.primaryColor}/>
+            </View>
+        )
+    }
 
     return (<FlatList
         data={products}
@@ -32,6 +47,8 @@ const styles = StyleSheet.create({
         flex: 1, backgroundColor: '#fff', alignItems: 'center', justifyContent: 'center',
     }, text: {
         fontSize: 20, fontFamily: 'SplineSans-Bold'
+    }, centered :{
+        flex:1,justifyContent:"center",alignItems:"center"
     }
 });
 export default ProductListScreen
